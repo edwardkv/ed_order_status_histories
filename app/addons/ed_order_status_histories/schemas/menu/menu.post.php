2e60fdbcf1ae1ae8d27cbd4ecbfd9e3ecabe2045
edwardkv <edwardkv@gmail.com>
2021-06-23 16:54:38 +0400

    add to schemas menu item

---
+++ app/addons/ed_order_status_histories/schemas/menu/menu.post.php
@@ -0,0 +1,9 @@
+<?php
+
+$schema['central']['orders']['items']['ed_order_status_histories'] = array(
+    'position' => 1000,
+    'href' => 'ed_order_status_histories.show',
+    'attrs' => ['class' => 'is-addon']
+);
+
+return $schema;
